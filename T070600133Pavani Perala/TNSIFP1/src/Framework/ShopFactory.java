093cfd585b4a00c07a0f1bfb76b846899c6846bf
pavani743 <154573756+pavani743@users.noreply.github.com>
2024-05-15 10:59:37 +0530

    Add files via upload

---
+++ T070600133Pavani Perala/TNSIFP1/src/Framework/ShopFactory.java
@@ -0,0 +1,7 @@
+package Framework;
+
+public abstract class ShopFactory {
+
+	public abstract PrimeAccount getNewPrimeAccount(int accNo, String accNm, float charges, boolean isPrime);
+	public abstract NormalAccount getNewNormalAccount(int accNo, String accNm, float charges, float deliveryCharges);
+}
