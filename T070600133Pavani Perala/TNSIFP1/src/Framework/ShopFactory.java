82f195a45b8b1224add6e658ab39b55715fd7473
pavani743 <154573756+pavani743@users.noreply.github.com>
2024-05-15 11:06:32 +0530

    Delete T070600133Pavani Perala/TNSIFP1 directory

--- T070600133Pavani Perala/TNSIFP1/src/Framework/ShopFactory.java
+++
@@ -1,7 +0,0 @@
-package Framework;
-
-public abstract class ShopFactory {
-
-	public abstract PrimeAccount getNewPrimeAccount(int accNo, String accNm, float charges, boolean isPrime);
-	public abstract NormalAccount getNewNormalAccount(int accNo, String accNm, float charges, float deliveryCharges);
-}
